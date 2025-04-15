be8a416991506341bd2a9bcb55fc69dcaf607567
Shomari <nyusternie@sdot.io>
2025-04-15 04:20:21 -0400

    Add async.

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export default {
 			'content-type': 'image/png'
 		}
 
-		return env.ASSETS.fetch('canvas.png')
+		return await env.ASSETS.fetch('canvas.png')
 		// const canvas = env.ASSETS.fetch(request)
 		// // return new Response('L1 NodΞRunr Lean Canvas')
 		// return new Response(JSON.stringify({ name: "Cloudflare" }), {
