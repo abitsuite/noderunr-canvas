28900c060a89bce9ccc235b395f63a72cd207ca2
Shomari <nyusternie@sdot.io>
2025-04-15 04:16:24 -0400

    Add media.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 export default {
+	/* Set headers. */
+	const headers = {
+		'content-type': 'image/png'
+	}
+
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('L1 NodΞRunr Lean Canvas')
+		return env.ASSETS.fetch('canvas.png')
+		// const canvas = env.ASSETS.fetch(request)
+		// // return new Response('L1 NodΞRunr Lean Canvas')
+		// return new Response(JSON.stringify({ name: "Cloudflare" }), {
+        // 	headers,
+      	// })
 	},
 } satisfies ExportedHandler<Env>
